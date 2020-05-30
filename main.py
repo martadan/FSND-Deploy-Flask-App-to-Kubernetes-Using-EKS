9c8d64fed7acb6320d056b81dbab77579473bfcc
Danil <dsm013@gmail.com>
2020-05-29 20:30:01 -0500

    Modified '/' response to fail unit test

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy, but throw an error please")
 
 
 @APP.route('/auth', methods=['POST'])
